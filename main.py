908f046d579c6e78a27a705a1d3563e370adf665
Dansonn75 <dsonnabend1@gmail.com>
2025-04-03 14:15:25 +0800

    Update main.py
    changed port

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ async def webhook(request: Request):
     return {"message": "Webhook received"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+   uvicorn.run(app, host="0.0.0.0", port=10000)
